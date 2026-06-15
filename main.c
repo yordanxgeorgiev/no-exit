ef53c35ee1dbd681c705e76af52c6f6ee63a951c
ygeorgiev <yordanxgeorgiev@abv.bg>
2026-06-15 17:57:36 +0300

    feat: add simple TCP server

--- main.c
+++ main.c
@@ -1,6 +1,31 @@
-#include <stdio.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <string.h>
 
-int main(int argc, char *argv[]){
-    printf("js sucks\n");
+#define LISTEN_BACKLOG 50
+
+int main(int argc, char *argv[])
+{
+    int fd;
+    struct sockaddr_in server_address, client_address;
+    socklen_t client_address_size = sizeof(client_address);
+
+    memset(&server_address, 0, sizeof(server_address));
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(9632);
+    server_address.sin_addr.s_addr = INADDR_ANY;
+
+    fd = socket(AF_INET, SOCK_STREAM, 0);
+    bind(fd, (struct sockaddr *)&server_address, sizeof(server_address));
+    listen(fd, LISTEN_BACKLOG);
+
+    while (1)
+    {
+        int client_fd = accept(fd, (struct sockaddr *)&client_address, &client_address_size);
+        close(client_fd);
+    }
+
+    close(fd);
     return 0;
 }
